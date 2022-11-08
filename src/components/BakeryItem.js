f2336b696678353c5264264f86d658745f1090d3
caffeinatedcapybara101 <angela_xing+cs1300@brown.edu>
2022-11-08 18:48:27 -0500

    finish studio

--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,13 @@
 // TODO: create a component that displays a single bakery item
+function createBakeryItem(props) {
+    return (
+        <div>
+            <img src={props.image} class="bakery-image" />
+            <div class="item-name">{props.name}</div>
+            <p class="item-description">{props.description}</p>
+            <div class="item-price">{props.price}</div>
+        </div>
+    );
+}
+
+export default createBakeryItem;
